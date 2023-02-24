fe9d852d4869c28a7ebfec46aca52ec065e26b14
Rafael Pacheco <rafahmp123@gmail.com>
2023-02-24 16:52:34 -0300

    Alterando Head

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import Head from "./Components/Head";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
 import Conta from "./Components/Conta/Conta";
 import Cartao from "./Components/Cartao/Cartao";
 import Duvidas from "./Components/Duvidas/Duvidas";
 import Footer from "./Components/Footer/Footer";
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Head
+        title="RPx Bank | O seu banco 100% digital desenvolvido com React"
+        description="RPx Bank, O seu banco 100% digital desenvolvido com React"
+      />
       <Header />
       <Hero />
       <Conta />
