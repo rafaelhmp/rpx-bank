d93b2f0e4d7a5cb29af2c047f9e1839ea2fabc74
Rafael Pacheco <rafahmp123@gmail.com>
2023-03-06 19:30:49 -0300

    Adicionando menu hamburguer mobile

--- src/Components/Header/MenuMobile/MenuMobile.js
+++ src/Components/Header/MenuMobile/MenuMobile.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { menuItens } from "../menuItens";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./MenuMobile.css";
 
 const MenuMobile = () => {
-  return <div>MenuMobile</div>;
+  const [menuActive, setMenuActive] = React.useState(false);
+  React.useEffect(() => {
+    document.body.style.overflowY = menuActive ? "hidden" : "auto";
+  }, [menuActive]);
+  return (
+    <div>
+      <FontAwesomeIcon
+        icon="fa-solid fa-bars"
+        onClick={() => setMenuActive(true)}
+      />
+      <div className={`hamburguer ${menuActive ? "show" : ""}`}>
+        <nav>
+          <FontAwesomeIcon
+            icon="fa-solid fa-x"
+            onClick={() => setMenuActive(false)}
+          />
+          {menuItens.map((item, index) => (
+            <a href="#" key={index}>
+              {item.title}
+            </a>
+          ))}
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default MenuMobile;
